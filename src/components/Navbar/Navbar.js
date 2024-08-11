9535c738d33577b5730ca993daf94428e9507a78
Zin Myo Win <mgzinmyowin12@gmail.com>
2024-08-11 20:21:37 +0700

    Arrange the responsive for every screen size possible

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,26 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import { RxHamburgerMenu } from "react-icons/rx";
 
 export default function Navbar() {
-  return (
-     
-      <div class='nav-container'>
-        <div class='nav-logo'>
-          <a href='#'>KAUNG HTIKE SAN</a>
-        </div>
-        <div class='nav-links-container'>
-          <div class='nav-link'>
-            <a href='#'>Work</a>
-          </div>
-          <div class='nav-link'>
-            <a href='#'>About</a>
-          </div>
-          <div class='nav-link'>
-            <a href='#'>Contact</a>
-          </div>
-        </div>
-      </div>
 
-     
+  const [showHamburgerMenu, setShowHamburgerMenu] = useState('')
+
+  const hamburgerMenu = ()=>{
+    setShowHamburgerMenu(prevState => prevState === 'active' ? '' : 'active');
+  }
+
+  return (
+      <div className="nav-bar">
+        <div className='nav-container'>
+          <div className='nav-logo'>
+            <a href='#'>KAUNG HTIKE SAN</a>
+          </div>
+          <div className='nav-links-container'>
+            <div className='nav-link'>
+              <a href='#'>Work</a>
+            </div>
+            <div className='nav-link'>
+              <a href='#'>About</a>
+            </div>
+            <div className='nav-link'>
+              <a href='#'>Contact</a>
+            </div>
+          </div>
+        
+          <RxHamburgerMenu onClick={hamburgerMenu} className="hamburger-menu"/>
+        
+        </div>
+
+        <div className={`hamburger-nav-links-container ${showHamburgerMenu}`}>
+            <div className='hamburger-nav-link'>
+              <a href='#'>Work</a>
+            </div>
+            <div className='hamburger-nav-link'>
+              <a href='#'>About</a>
+            </div>
+            <div className='hamburger-nav-link'>
+              <a href='#'>Contact</a>
+            </div>
+          </div>
+      </div>
   );
 }
